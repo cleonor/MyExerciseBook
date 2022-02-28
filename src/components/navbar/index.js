173788853be20ef96ece64145ab4ffedbc3e6083
Leonor <leonorcascais@hotmail.com>
2022-02-28 11:29:17 +0100

    add navbar

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <div>
-            <h1>Welcome</h1>
-        </div>
+        <Navbar>
+            <Container>
+                <Navbar.Brand href="#home">MyExerciseBook</Navbar.Brand>
+                <NavMenu>
+                    <NavLink to="/1">Exercise1</NavLink>
+                    <NavLink to="/2">Exercise2</NavLink>
+                    <NavLink to="/3">Exercise3</NavLink>
+                </NavMenu>
+            </Container>
+        </Navbar>
     )
 }
 
