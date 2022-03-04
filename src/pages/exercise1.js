fea575e7aeb921472709b0abe1dc1bd286472b75
Leonor <leonorcascais@hotmail.com>
2022-03-04 19:32:25 +0100

    create class component to exercise 1

--- src/pages/exercise1.js
+++ src/pages/exercise1.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import ButtonComponent from './buttonComponent'
 
-const Exercise1 = () => {
-    return (
-        <div>
-            <h1>Exercise 1</h1>
-        </div>
-    );
+class Exercise1 extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { actualNumber: 4 };
+    }
+
+    changeValueOneMore = (value) => {
+        this.setState({
+            actualNumber: this.state.actualNumber + value
+        })
+    }
+
+    changeValueOneLess() {
+        this.changeValueOneMore(-1)
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>
+                    Actual number: {this.state.actualNumber}
+                </h2>
+
+                <ButtonComponent
+                    className={"subButton"}
+                    onClick={this.changeValueOneLess.bind(this)}
+                    title={"-1"}
+                />
+
+                <ButtonComponent
+                    className={"summButton"}
+                    onClick={() => this.changeValueOneMore(1)}
+                    title={"+1"}
+                />
+            </div >
+        )
+    }
 };
 
 export default Exercise1;
