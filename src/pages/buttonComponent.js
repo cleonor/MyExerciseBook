352e639b6454be7fa4c2c9af3fe770bb88ecddbb
Leonor <leonorcascais@hotmail.com>
2022-03-08 18:15:05 +0100

    apply mui button

--- src/pages/buttonComponent.js
+++ src/pages/buttonComponent.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Button from '@mui/material/Button';
 
 const ButtonComponent = ({ className, onClick, title }) => {
 
     return (
-        <button type="button" className={className} onClick={onClick}> {title} </button>
+        <Button
+            variant="contained"
+            type="button"
+            className={className}
+            onClick={onClick}
+        >
+            {title}
+        </Button>
     )
 }
 
