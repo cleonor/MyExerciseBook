67b781587d6e590e8f8e7ff7cc52c9fa23b3343e
Leonor <leonorcascais@hotmail.com>
2022-03-18 21:17:15 +0100

    import image to exercise

--- src/pages/exercise2.js
+++ src/pages/exercise2.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './exercise2.css'
+import './exercise2.css';
+import github from './../images/github.png'
 
 const Exercise2 = () => {
     return (
         <div>
-            <button classname="exercise2">
+            <img src={github} alt="Github"></img>
+            <button
+                classname="exercise2"
+            >
                 Click me!
             </button>
         </div>
