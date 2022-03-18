ab726dbed9aab3f9f23aad182b5c37c7739fe5ff
Leonor <leonorcascais@hotmail.com>
2022-03-18 21:30:09 +0100

    display image onClick

--- src/pages/exercise2.js
+++ src/pages/exercise2.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './exercise2.css';
 import github from './../images/github.png'
 
 const Exercise2 = () => {
+
+    const [isVisible, setVisible] = useState(false);
+
     return (
         <div>
-            <img src={github} alt="Github"></img>
+            {isVisible ? (<img
+                src={github}
+                alt="Github"
+                isVisible={isVisible}
+                setVisible={setVisible}
+            >
+            </img>) : null }
             <button
                 classname="exercise2"
+                onClick={() => setVisible(true)}
             >
                 Click me!
             </button>
