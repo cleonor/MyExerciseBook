e524e5d2b0bb655674448479c19a3dc4b445f328
Leonor <leonorcascais@hotmail.com>
2022-03-18 22:26:43 +0100

    count +1 onClick

--- src/pages/exercise2.js
+++ src/pages/exercise2.js
@@ -5,23 +5,30 @@ import github from './../images/github.png'
 const Exercise2 = () => {
 
     const [isVisible, setVisible] = useState(false);
+    const [imageNumber, setImageNumber] = useState(0);
+    console.log(imageNumber);
 
     return (
         <div>
             {isVisible ? (<img
+                className="imageExercise2"
                 src={github}
                 alt="Github"
                 isVisible={isVisible}
                 setVisible={setVisible}
             >
-            </img>) : null }
+            </img>) : null}
             <button
-                classname="exercise2"
-                onClick={() => setVisible(true)}
+                className="buttonExercise2"
+                onClick={() => {
+                    setVisible(true);
+                    setImageNumber(imageNumber + 1);
+                }
+                }
             >
                 Click me!
             </button>
-        </div>
+        </div >
     )
 }
 
